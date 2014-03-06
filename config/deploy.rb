9b5d0e787e595666e70e50f88400c8ed415f630e
shiguodong <oreatial@gmail.com>
2014-03-06 10:22:17 +0800

    add asset

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,6 +55,15 @@ set(:symlinks, [
     link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
   },
   {
+  	source: ".ruby-version",
+    link: "/#{release_path}/.ruby-version"
+  },
+  {
+  	source: ".rbenv-gemsetsn",
+    link: "/#{release_path}/.rbenv-gemsets"
+  },
+
+  {
   	source: "config/database.yml",
     link: "/#{release_path}/config/#{fetch(:full_app_name)}"
   },
@@ -84,51 +93,4 @@ namespace :deploy do
   
 end
 
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
-
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-#
 
